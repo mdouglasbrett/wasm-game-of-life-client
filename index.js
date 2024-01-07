91567f0ca98823398f854bf4bb00e748f7b55ace
Michael Brett <mdouglasbrett@icloud.com>
2024-01-07 16:04:33 +0000

    Adds fps readout

--- index.js
+++ index.js
@@ -128,6 +128,51 @@ const drawGlider = (row, col) => {
     universe.toggle(row - 1, col - 1);
 };
 
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimestamp = performance.now();
+    }
+
+    render() {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimestamp;
+        this.lastFrameTimestamp = now;
+        const fps = 1 / delta * 1000;
+
+        this.frames.push(fps);
+
+        if (this.frames.length > 100) {
+            // moves array along by one, earliest entry gone
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+
+        let sum = 0;
+
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+
+        let mean = sum / this.frames.length;
+
+        this.fps.textContent = `
+        Frames per second:
+        latest = ${Math.round(fps)}
+        avg of last 100 = ${Math.round(mean)}
+        min of last 100 = ${Math.round(min)}
+        max of last 100 = ${Math.round(max)}
+        `.trim();
+
+    }
+        
+}
+
 canvas.addEventListener("click", (e) => {
     console.log(e);
     const boundingRect = canvas.getBoundingClientRect();
@@ -163,6 +208,7 @@ const isPaused = () => {
 };
 
 const renderLoop = () => {
+    fps.render();
     //debugger;
     universe.tick();
 
